9fca11e52d1d88f470a2445bbd803924a39119fe
Thomas Rughooputh <trughooputh@users.noreply.github.com>
2021-02-27 18:59:18 +0100

    Fix src imgs

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -7,14 +7,14 @@ class Card extends React.Component {
       <div className="card">
         <div className="card-image">
           <figure className="image is-4by3">
-            <img src="`${item.image}`" alt="{item.name}" />
+            <img src={item.image} alt={item.name} />
           </figure>
         </div>
         <div className="card-content">
           <div className="media">
             <div className="media-left">
               <figure className="image is-48x48">
-                <img src="`${item.image}`" alt="{item.name}" />
+                <img src={item.image} alt={item.name} />
               </figure>
             </div>
             <div className="media-content">
